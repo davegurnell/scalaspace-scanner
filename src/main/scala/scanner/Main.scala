b9c08c3e391c6dbf34ad251439bd7138e8c1075c
Dave Gurnell <dave@untyped.com>
2015-07-18 12:35:32 +0100

    Parse JSON as a `Seq[MeetupGroup]`

---
+++ src/main/scala/scanner/Main.scala
@@ -0,0 +1,20 @@
+package scanner
+
+import dispatch._
+import dispatch.Defaults._
+
+import scala.concurrent._
+import scala.concurrent.duration._
+
+import upickle._
+import upickle.default._
+
+object Main extends App {
+  val svc = url("https://api.meetup.com/find/groups?key=1e7567444f71b28447db5b1211d60&text=Scala&radius=global&sign=true")
+
+  val json: String = Await.result(Http(svc OK as.String), 10.seconds)
+
+  val groups = read[Seq[MeetupGroup]](json)
+
+  groups.foreach(println)
+}
---
+++ src/main/scala/scanner/data.scala
@@ -0,0 +1,29 @@
+package scanner
+
+case class MeetupGroup(
+  id: Int,
+  name: String,
+  link: String,
+  city: String,
+  country: String,
+  description: String,
+  lat: Double,
+  lon: Double,
+  members: Int,
+  organizer: Organizer,
+  // group_photo // for later maybe
+  category: Category,
+  score: Double
+)
+
+case class Organizer(
+  id: Int,
+  name: String,
+  bio: String
+)
+
+case class Category(
+  id: Int,
+  name: String,
+  shortname: String
+)
