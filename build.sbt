ad69d0fa73d7fd162f36fdd51042ab4639743eed
Dave Gurnell <dave@untyped.com>
2015-07-18 12:10:40 +0100

    Load JSON from meetup.com api and print it!

--- build.sbt
+++ build.sbt
@@ -1,3 +1,11 @@
 name := "scalaspace-scanner"
 
 version := "0.1"
+
+scalaVersion := "2.11.6"
+
+libraryDependencies ++= Seq(
+  "net.databinder.dispatch" %% "dispatch-core" % "0.11.2",
+  "com.lihaoyi" %% "upickle" % "0.3.4"
+)
+
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,18 @@
+import dispatch._
+import dispatch.Defaults._
+
+import scala.concurrent._
+import scala.concurrent.duration._
+
+import upickle._
+import upickle.default._
+
 object Main extends App {
-  println("Hi")
+  val svc = url("https://api.meetup.com/find/groups?key=1e7567444f71b28447db5b1211d60&country=uk&sign=true")
+
+  val json: String = Await.result(Http(svc OK as.String), 1.seconds)
+
+  val jsValue: Js.Value = upickle.json.read(json)
+
+  println(jsValue)
 }
